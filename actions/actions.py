bf02c4b400b1d8f8fd19cb043c7dd4d00b2ed723
Máté Vass <vass.mate99@gmail.com>
2025-04-06 21:03:28 +0200

    custom welcome message

--- actions/actions.py
+++ actions/actions.py
@@ -1,18 +1,25 @@
-# This files contains your custom actions which can be used to run
-# custom Python code.
-#
-# See this guide on how to implement these action:
-# https://rasa.com/docs/rasa/custom-actions
+from typing import Any, Text, Dict, List
+from rasa_sdk import Action, Tracker
+from rasa_sdk.events import SessionStarted, ActionExecuted, EventType
+from rasa_sdk.executor import CollectingDispatcher
+
+class ActionSessionStart(Action):
+    def name(self) -> Text:
+        return "action_session_start"
+
+    async def run(
+        self,
+        dispatcher: CollectingDispatcher,
+        tracker: "Tracker",
+        domain: Dict[Text, Any],
+    ) -> List[EventType]:
+
+        events = [SessionStarted(), ActionExecuted("action_listen")]
+        dispatcher.utter_message(text="Hi there! I'm your assistant. How can I help you today?")
+
+        return events
 
 
-# This is a simple example for a custom action which utters "Hello World!"
-
-# from typing import Any, Text, Dict, List
-#
-# from rasa_sdk import Action, Tracker
-# from rasa_sdk.executor import CollectingDispatcher
-#
-#
 # class ActionHelloWorld(Action):
 #
 #     def name(self) -> Text:
